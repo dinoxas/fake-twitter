2640e89e4b2e23ffdeb095eee99f46fd8e729a42
dinoxas <dhassiotis07@gmail.com>
2020-02-16 19:08:21 +0000

    Created reducers and passed them into combineReducer method of Redux

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import { users } from "../reducers/users";
+import { tweets } from "../reducers/tweets";
+import { authedUser } from "../reducers/authedUser";
+
+export default combineReducers({
+  authedUser,
+  users,
+  tweets
+});
