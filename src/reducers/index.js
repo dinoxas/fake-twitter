12991c52cb7cf255e7cf6c5f2e440ab898b73a08
dinoxas <dhassiotis07@gmail.com>
2020-02-17 13:56:48 +0000

    created logger middleware and Redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
-import { users } from "../reducers/users";
-import { tweets } from "../reducers/tweets";
-import { authedUser } from "../reducers/authedUser";
+import users from "../reducers/users";
+import tweets from "../reducers/tweets";
+import authedUser from "../reducers/authedUser";
 
 export default combineReducers({
   authedUser,
