42d8ed6dd5ef7616bfe82cefe427100c3b0e0842
dinoxas <dhassiotis07@gmail.com>
2020-02-19 12:44:46 +0000

    Created a TweetPage wrapper component to render tweets and replies and updated the tweet reducer

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -2,31 +2,11 @@ import { RECEIVE_TWEETS, TOGGLE_TWEET, ADD_TWEET } from "../actions/tweets";
 
 export default function tweets(state = {}, action) {
   switch (action.type) {
-    case ADD_TWEET:
-      const { tweet } = action;
-
-      let replyingTo = {};
-      if (tweet.replyingTo !== null) {
-        replyingTo = {
-          [tweet.replyingTo]: {
-            ...state[tweet.replyingTo],
-            replies: state[tweet.replyTo].replies.concat([tweet.id])
-          }
-        };
-      }
-
-      return {
-        ...state,
-        [action.tweet.id]: action.tweet,
-        ...replyingTo
-      };
-
     case RECEIVE_TWEETS:
       return {
         ...state,
         ...action.tweets
       };
-
     case TOGGLE_TWEET:
       return {
         ...state,
@@ -37,6 +17,24 @@ export default function tweets(state = {}, action) {
               ? state[action.id].likes.filter(uid => uid !== action.authedUser)
               : state[action.id].likes.concat([action.authedUser])
         }
+      };
+    case ADD_TWEET:
+      const { tweet } = action;
+
+      let replyingTo = {};
+      if (tweet.replyingTo !== null) {
+        replyingTo = {
+          [tweet.replyingTo]: {
+            ...state[tweet.replyingTo],
+            replies: state[tweet.replyingTo].replies.concat([tweet.id])
+          }
+        };
+      }
+
+      return {
+        ...state,
+        [action.tweet.id]: action.tweet,
+        ...replyingTo
       };
     default:
       return state;
